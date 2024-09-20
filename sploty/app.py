2a684d57316bf8462825b428ff55d8edfe3426c2
Gudsfile <gudsfile@protonmail.com>
2024-09-21 01:03:21 +0200

    ♻️ filter to app

--- sploty/app.py
+++ sploty/app.py
@@ -9,6 +9,7 @@ from sploty.settings import logger
 
 class Arguments(BaseModel):
     resources_path: str = Field(description="a required string")
+    previous_enriched_streaming_history_path: str = Field(None, description="an optional string")
 
 
 def main() -> None:
@@ -27,9 +28,17 @@ def main() -> None:
     streaming_history_paths = list(Path(resources_path).glob("Streaming_History_Audio_*.json"))
 
     concated_streaming_history_path = Path(f"{resources_path}/sploty_concated_history.csv")
+    to_enrich_streaming_history_path = Path(f"{resources_path}/sploty_filtered_history.csv")
+    enriched_streaming_history_path = Path(f"{resources_path}/sploty_enriched_history.csv")
     # Process
     logger.info("============== CONCAT ==============")
     concat.main(streaming_history_paths, concated_streaming_history_path)
+    logger.info("============== FILTER ==============")
+    filter.main(
+        concated_streaming_history_path,
+        to_enrich_streaming_history_path,
+        args.previous_enriched_streaming_history_path or enriched_streaming_history_path,
+    )
 
 
 if __name__ == "__main__":
--- sploty/filter.py
+++ sploty/filter.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import json
 from pathlib import Path
 
@@ -5,46 +7,55 @@ import pandas as pd
 from pandas.errors import EmptyDataError
 from settings import logger
 
-CONFIG_FILE = "config.json"
-with Path(CONFIG_FILE).open(encoding="utf8") as file:
-    CONFIG = json.load(file)
 
-# Files
-RESOURCES_FOLDER = CONFIG["file"]["resources_folder"]
+def main(concated_path: list, to_enrich_path: str, enriched_path: str | None = None):
+    """
+    concated_path: file where read concated streaming history
+    to_enrich_path: file to write filtered streaming history
+    enriched_path: file where already enriched streaming history
+    """
+    # Read track history Spotify file
+    df_stream = pd.read_csv(concated_path)
+    logger.info("%i rows to enrich", len(df_stream))
 
-YOUR_STREAMING_HISTORY_FILES = "StreamingHistory*.json"
-YOUR_STREAMING_HISTORY_PATHS = list(Path(RESOURCES_FOLDER).glob(YOUR_STREAMING_HISTORY_FILES))
+    try:
+        df_already_enriched = pd.read_csv(enriched_path)
+        logger.info("%i enriched rows found", len(df_already_enriched))
+        # TODO there is an error : it lost new stream of a previously enriched track
+        # df_stream = df_stream[(~df_stream.track_uri.isin(df_already_enriched.track_uri)) | (df_stream.end_time > max(df_already_enriched.end_time))] # noqa: ERA001, E501
+        # df_stream = df_stream[(~df_stream.id.isin(df_already_enriched.id))]  noqa: ERA001
+        df_stream = df_stream[df_stream.end_time > max(df_already_enriched.end_time)]
+        logger.info("only %i rows to enrich", len(df_stream))
+    except EmptyDataError:
+        logger.warning("empty backup file found (%s)", enriched_path)
+    except FileNotFoundError:
+        logger.warning("no backup file found (%s)", enriched_path)
 
-ALL_YOUR_STREAMING_HISTORY_FILE = "AllStreamingHistory.csv"
-ALL_YOUR_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + ALL_YOUR_STREAMING_HISTORY_FILE
+    # Drop NaN row
+    df_stream = df_stream[df_stream["track_uri"].notna()]
+    logger.info("%i rows to enrich without empty track_uri", len(df_stream))
 
-ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_FILE = "AllStreamingHistoryToEnrich.csv"
-ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_PATH = RESOURCES_FOLDER + "/" + ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_FILE
+    # Save stream to enrich
+    df_stream.to_csv(to_enrich_path, mode="w", index=False)
+    logger.info("%i rows are saved at %s", len(df_stream), to_enrich_path)
 
-YOUR_ENRICHED_STREAMING_HISTORY_FILE = "AllEnrichedStreamingHistory.csv"
-YOUR_ENRICHED_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + YOUR_ENRICHED_STREAMING_HISTORY_FILE
 
-# Read track history Spotify file
-df_stream = pd.read_csv(ALL_YOUR_STREAMING_HISTORY_PATH)
-logger.info("%i rows to enrich", len(df_stream))
+if __name__ == "__main__":
+    CONFIG_FILE = "config.json"
 
-try:
-    df_already_enriched = pd.read_csv(YOUR_ENRICHED_STREAMING_HISTORY_PATH)
-    logger.info("%i enriched rows found", len(df_already_enriched))
-    # TODO there is an error : it lost new stream of a previously enriched track
-    # df_stream = df_stream[(~df_stream.track_uri.isin(df_already_enriched.track_uri)) | (df_stream.end_time > max(df_already_enriched.end_time))] noqa: ERA001
-    # df_stream = df_stream[(~df_stream.id.isin(df_already_enriched.id))]  noqa: ERA001
-    df_stream = df_stream[df_stream.end_time > max(df_already_enriched.end_time)]
-    logger.info("only %i rows to enrich", len(df_stream))
-except EmptyDataError:
-    logger.warning("empty backup file found (%s)", YOUR_ENRICHED_STREAMING_HISTORY_PATH)
-except FileNotFoundError:
-    logger.warning("no backup file found (%s)", YOUR_ENRICHED_STREAMING_HISTORY_PATH)
+    with Path(CONFIG_FILE).open(encoding="utf8") as file:
+        CONFIG = json.load(file)
 
-# Drop NaN row
-df_stream = df_stream[df_stream["track_uri"].notna()]
-logger.info("%i rows to enrich without empty track_uri", len(df_stream))
+    # Files
+    RESOURCES_FOLDER = CONFIG["file"]["resources_folder"]
 
-# Save stream to enrich
-df_stream.to_csv(ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_PATH, mode="w", index=False)
-logger.info("%i rows are saved at %s", len(df_stream), ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_PATH)
+    ALL_STREAMING_HISTORY_FILE = "AllStreamingHistory.csv"
+    ALL_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + ALL_STREAMING_HISTORY_FILE
+
+    ALL_STREAMING_HISTORY_TO_ENRICH_FILE = "AllStreamingHistoryToEnrich.csv"
+    ALL_STREAMING_HISTORY_TO_ENRICH_PATH = RESOURCES_FOLDER + "/" + ALL_STREAMING_HISTORY_TO_ENRICH_FILE
+
+    ENRICHED_STREAMING_HISTORY_FILE = "AllEnrichedStreamingHistory.csv"
+    ENRICHED_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + ENRICHED_STREAMING_HISTORY_FILE
+
+    main(ALL_STREAMING_HISTORY_FILE, ALL_STREAMING_HISTORY_TO_ENRICH_FILE, ENRICHED_STREAMING_HISTORY_PATH)
