63cbb4da799ca21a33f20edd7d404e6e61731c97
Gudsfile <gudsfile@protonmail.com>
2024-09-21 13:12:44 +0200

    ♻️ audio features to app

--- sploty/audio_features.py
+++ sploty/audio_features.py
@@ -6,55 +6,29 @@ from pathlib import Path
 
 import pandas as pd
 import requests
+from pydantic import BaseModel, HttpUrl
 from requests.exceptions import HTTPError
 from settings import logger
 from tinydb import TinyDB
 
-CONFIG_FILE = "config.json"
-with Path(CONFIG_FILE).open(encoding="utf8") as file:
-    CONFIG = json.load(file)
 
-CHUNK_SIZE = CONFIG["file"]["chunk_size"]
-
-# Files
-RESOURCES_FOLDER = CONFIG["file"]["resources_folder"]
-ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_FILE = "AllStreamingHistoryToEnrich.csv"
-ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_PATH = RESOURCES_FOLDER + "/" + ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_FILE
-
-# Some rows are already enriched with audio features, some aren't
-YOUR_ENRICHED_STREAMING_HISTORY_FILE = "AllEnrichedStreamingHistory.csv"
-YOUR_ENRICHED_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + YOUR_ENRICHED_STREAMING_HISTORY_FILE
-
-# Spotify's authentication and config
-SPOTIFY_CLIENT_ID = CONFIG["spotify"]["client_id"]
-SPOTIFY_CLIENT_SECRET = CONFIG["spotify"]["client_secret"]
-SPOTIFY_AUTH_URL = CONFIG["spotify"]["auth_url"]
-SPOTIFY_BASE_URL = CONFIG["spotify"]["base_url"]
-SPOTIFY_SLEEP = CONFIG["spotify"]["s_sleep"]
-SPOTIFY_TIMEOUT = CONFIG["spotify"]["timeout"]
-
-auth_response = requests.post(
-    SPOTIFY_AUTH_URL,
-    {
-        "grant_type": "client_credentials",
-        "client_id": SPOTIFY_CLIENT_ID,
-        "client_secret": SPOTIFY_CLIENT_SECRET,
-    },
-    timeout=SPOTIFY_TIMEOUT,
-)
-auth_response_data = auth_response.json()
-
-ACCESS_TOKEN = auth_response_data["access_token"]
-SPOTIFY_HEADERS = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
-
-# TinyDb
-DB_PATH = CONFIG["tinydb"]["track_audio_features"]
-DB = TinyDB(DB_PATH)
+class SpotifyApiParams(BaseModel):
+    base_url: str
+    endpoint: str
+    url: HttpUrl
+    headers: dict
+    timeout: float
+    sleep: float
 
 
-def do_spotify_request(url, headers, params=None):
+def do_spotify_request(spotify_api_params: SpotifyApiParams, params=None):
     try:
-        response = requests.get(url, headers=headers, params=params, timeout=120)
+        response = requests.get(
+            spotify_api_params.url,
+            headers=spotify_api_params.headers,
+            params=params,
+            timeout=spotify_api_params.timeout,
+        )
         logger.debug(" -> %s", response.request.url)
         logger.debug(
             " <- %i %s",
@@ -65,16 +39,16 @@ def do_spotify_request(url, headers, params=None):
         return response.json()
     except HTTPError as err:
         if err.response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-            logger.warning("HTTPError - %s (sleeping %is...)", err, SPOTIFY_SLEEP)
-            time.sleep(SPOTIFY_SLEEP)
-            return do_spotify_request(url, headers, params)
+            logger.warning("HTTPError - %s (sleeping %is...)", err, spotify_api_params.sleep)
+            time.sleep(spotify_api_params.sleep)
+            return do_spotify_request(spotify_api_params, params)
         logger.warning("HTTPError - %s (skipping)", err)
         raise
 
 
-def get_track_audio_features(track_uris: list):
+def get_track_audio_features(spotify_api_params: SpotifyApiParams, track_uris):
     params = [("ids", ",".join(track_uris)), ("type", "track"), ("market", "FR")]
-    response = do_spotify_request(SPOTIFY_BASE_URL + "audio-features/", headers=SPOTIFY_HEADERS, params=params)
+    response = do_spotify_request(spotify_api_params, params=params)
     return response["audio_features"]
 
 
@@ -82,9 +56,9 @@ def inserts_in_db(db, data):
     db.insert_multiple(data)
 
 
-def inserts_enriched_tracks(db, tracks_uri, chunk_size):
+def inserts_enriched_tracks(db, tracks_uri, chunk_size, spotify_api_params):
     for chunk in batched(tracks_uri, chunk_size):
-        track_audio_features = get_track_audio_features(chunk)
+        track_audio_features = get_track_audio_features(spotify_api_params, chunk)
         track_audio_features_without_none_value = [taf for taf in track_audio_features if taf]
         inserts_in_db(db, track_audio_features_without_none_value)
 
@@ -114,33 +88,98 @@ def completes_streams_with_audio_features(df_left, left_key, df_right, right_key
 # db.remove(doc_ids=dup_id)#noqa: ERA001
 # len(db.all())#noqa: ERA001
 
-# get the audio features of tracks saved it in the TinyDb
-df_stream = pd.read_csv(ALL_YOUR_STREAMING_HISTORY_TO_ENRICH_PATH)
-logger.info("%i streams", len(df_stream))
 
-track_uris_from_file = set(df_stream["track_uri"])
-track_uris_from_db = [track["id"] for track in DB.all()]
-audio_feature_uris_to_get = track_uris_from_file.difference(track_uris_from_db)
+def main(to_enrich_path: list, enriched_path: str, chunk_size: int, spotify_api_params: SpotifyApiParams, db: TinyDB):
+    # get the audio features of tracks saved it in the TinyDb
+    df_stream = pd.read_csv(to_enrich_path)
+    logger.info("%i streams", len(df_stream))
 
-logger.info("%i unique uris", len(track_uris_from_file))
-logger.info("%i uris for which the audio features are already retrieved", len(track_uris_from_db))
-logger.info("%i uris for which the audio features must be recovered", len(audio_feature_uris_to_get))
+    track_uris_from_file = set(df_stream["track_uri"])
+    track_uris_from_db = [track["id"] for track in db.all()]
+    audio_feature_uris_to_get = track_uris_from_file.difference(track_uris_from_db)
 
-inserts_enriched_tracks(DB, audio_feature_uris_to_get, CHUNK_SIZE)
+    logger.info("%i unique uris", len(track_uris_from_file))
+    logger.info("%i uris for which the audio features are already retrieved", len(track_uris_from_db))
+    logger.info("%i uris for which the audio features must be recovered", len(audio_feature_uris_to_get))
 
-track_uris_from_db = [track["id"] for track in DB.all()]
-track_uris_failed_to_get = audio_feature_uris_to_get.difference(track_uris_from_db)
-logger.info("%i uris for which the audio features are now retrieved", len(track_uris_from_db))
-logger.info("%i uris for which the audio features could not be recovered", len(track_uris_failed_to_get))
+    inserts_enriched_tracks(db, audio_feature_uris_to_get, chunk_size, spotify_api_params)
 
-# add audio feature to rows that do not have it
-df_enriched_streams = pd.read_csv(YOUR_ENRICHED_STREAMING_HISTORY_PATH)
-df_audio_features = pd.DataFrame(DB.all())
+    track_uris_from_db = [track["id"] for track in db.all()]
+    track_uris_failed_to_get = audio_feature_uris_to_get.difference(track_uris_from_db)
+    logger.info("%i uris for which the audio features are now retrieved", len(track_uris_from_db))
+    logger.info("%i uris for which the audio features could not be recovered", len(track_uris_failed_to_get))
 
-df_completed_streams = completes_streams_with_audio_features(df_enriched_streams, "track_uri", df_audio_features, "id")
-df_completed_streams.to_csv(YOUR_ENRICHED_STREAMING_HISTORY_PATH, mode="w", index=False)
-logger.info(
-    "%i rows are re-saved at %s with audio features completed",
-    len(df_completed_streams),
-    YOUR_ENRICHED_STREAMING_HISTORY_PATH,
-)
+    # add audio feature to rows that do not have it
+    df_enriched_streams = pd.read_csv(enriched_path)
+    df_audio_features = pd.DataFrame(db.all())
+
+    df_completed_streams = completes_streams_with_audio_features(
+        df_enriched_streams,
+        "track_uri",
+        df_audio_features,
+        "id",
+    )
+    df_completed_streams.to_csv(enriched_path, mode="w", index=False)
+    logger.info(
+        "%i rows are re-saved at %s with audio features completed",
+        len(df_completed_streams),
+        enriched_path,
+    )
+
+
+def get_spotify_auth_header(auth_url, client_id, client_secret, timeout):
+    auth_response = requests.post(
+        auth_url,
+        {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret,
+        },
+        timeout=timeout,
+    )
+    auth_response_data = auth_response.json()
+    return {"Authorization": f"Bearer {auth_response_data['access_token']}"}
+
+
+def get_db(path):
+    return TinyDB(path)
+
+
+if __name__ == "__main__":
+    CONFIG_FILE = "config.json"
+    with Path(CONFIG_FILE).open(encoding="utf8") as file:
+        CONFIG = json.load(file)
+
+    CHUNK_SIZE = CONFIG["file"]["chunk_size"]
+
+    # Files
+    RESOURCES_FOLDER = CONFIG["file"]["resources_folder"]
+    ALL_STREAMING_HISTORY_TO_ENRICH_FILE = "AllStreamingHistoryToEnrich.csv"
+    ALL_STREAMING_HISTORY_TO_ENRICH_PATH = RESOURCES_FOLDER + "/" + ALL_STREAMING_HISTORY_TO_ENRICH_FILE
+
+    # Some rows are already enriched with audio features, some aren't
+    ENRICHED_STREAMING_HISTORY_FILE = "AllEnrichedStreamingHistory.csv"
+    ENRICHED_STREAMING_HISTORY_PATH = RESOURCES_FOLDER + "/" + ENRICHED_STREAMING_HISTORY_FILE
+
+    # Spotify's authentication and config
+    SPOTIFY_CLIENT_ID = CONFIG["spotify"]["client_id"]
+    SPOTIFY_CLIENT_SECRET = CONFIG["spotify"]["client_secret"]
+    SPOTIFY_AUTH_URL = CONFIG["spotify"]["auth_url"]
+    SPOTIFY_BASE_URL = CONFIG["spotify"]["base_url"]
+    SPOTIFY_SLEEP = CONFIG["spotify"]["s_sleep"]
+    SPOTIFY_TIMEOUT = CONFIG["spotify"]["timeout"]
+
+    SPOTIFY_API_PARAMS = SpotifyApiParams(
+        base_url=SPOTIFY_BASE_URL,
+        endpoint="tracks",
+        url=f"{SPOTIFY_BASE_URL}/audio-features/",
+        headers=get_spotify_auth_header(SPOTIFY_AUTH_URL, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_TIMEOUT),
+        timeout=SPOTIFY_TIMEOUT,
+        sleep=SPOTIFY_SLEEP,
+    )
+
+    # TinyDb
+    DB_PATH = CONFIG["tinydb"]["track_audio_features"]
+    DB = TinyDB(DB_PATH)
+
+    main(ALL_STREAMING_HISTORY_TO_ENRICH_PATH, ENRICHED_STREAMING_HISTORY_PATH, CHUNK_SIZE, SPOTIFY_API_PARAMS, DB)
