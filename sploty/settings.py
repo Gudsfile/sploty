b35b9cd77d90bbce86ae43da4a660c739847ecdd
Gudsfile <gudsfile@protonmail.com>
2024-09-21 12:34:42 +0200

    ♻️ enrich to app

--- sploty/settings.py
+++ sploty/settings.py
@@ -2,7 +2,21 @@ from __future__ import annotations
 
 import logging
 import re
+from enum import Enum
 from typing import ClassVar
+
+
+class BoldColor(str, Enum):
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[36m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
 
 
 class CustomFormatter(logging.Formatter):
